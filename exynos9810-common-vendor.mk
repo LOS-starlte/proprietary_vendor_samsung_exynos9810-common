2096b8f8322ed276ca62b1e2d942f5a659f8fa4b
AnLiEsD <94976677+AnLiEsD@users.noreply.github.com>
2024-08-06 22:00:26 +0200

    exynos9810-common: drop non necessary prebuilts the source code for these prebuilts are included in the hardware samsung repo on the lineageos-21 branch
    Signed-off-by: AnLiEsD <94976677+AnLiEsD@users.noreply.github.com>

--- exynos9810-common-vendor.mk
+++ exynos9810-common-vendor.mk
@@ -194,10 +194,7 @@ PRODUCT_PACKAGES += \
     libsec-ril \
     libskeymaster3device \
     vendor.samsung.hardware.gnss@2.0 \
-    vendor.samsung.hardware.radio.bridge@2.0 \
     vendor.samsung.hardware.radio.channel@2.0 \
-    vendor.samsung.hardware.radio@2.0 \
-    vendor.samsung.hardware.radio@2.1 \
     vendor.samsung.hardware.security.widevine.keyprovisioning@1.0 \
     libaptX_encoder \
     snap_gpu_kernel_64 \
